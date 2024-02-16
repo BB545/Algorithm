deb2b18423234bbd6282c0d4c7ca2ca609dea578
BB545 <148606294+BB545@users.noreply.github.com>
2024-02-16 20:13:28 +0900

    Update JadenCase 문자열 만들기.py

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -7,7 +7,3 @@ def solution(s):
         else:
             a.append(i)
     return " ".join(a)
-
-#title : 문장에서 단어별 첫문자 대문자로 바꿈, 나머지는 소문자
-#capitalize : 문장이나 단어에서 첫문자 대문자로, 나머지는 소문자
-#s.split(" ") : 추가적인 공백을 별도로 분리해 저장가능
