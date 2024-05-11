6334d0b9d90c0f3175cb20c9ab4d67e12203ff10
노현희 <148606294+BB545@users.noreply.github.com>
2024-05-11 09:12:02 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,14 +1,14 @@
 def solution(n, computers):
     answer = 0
-    visited = [False]*n #초기 리스트 : 하나도 방문하지 않은
-    def dfs(pc): #활용할 dfs함수 정의
-        visited[pc]=True
-        for idx in range(n): #인접노드
-            if not visited[idx] and computers[pc][idx]: #방문하지 않았고, 인접할때
-                dfs(idx) #해당 노드 기준으로 다시 dfs탐색 진행
+    visited = [False]*n
+    
+    def dfs(pc):
+        visited[pc] = True
+        for idx in range(n):
+            if not visited[idx] and computers[pc][idx] == 1:
+                dfs(idx)
     for node in range(n):
         if not visited[node]:
             dfs(node)
             answer += 1
-    print(visited)
     return answer
