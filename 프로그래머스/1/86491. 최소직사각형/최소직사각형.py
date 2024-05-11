a99c443067241c0fb2bbe4d65c2d83d581198ce1
노현희 <148606294+BB545@users.noreply.github.com>
2024-05-11 09:42:23 +0900

    [level 1] Title: 최소직사각형, Time: 4.51 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,11 +1,13 @@
 def solution(sizes):
     max_w = 0
     max_h = 0
-
+    
     for size in sizes:
         size.sort()
-        w, h = size
-        max_w = max(max_w, w)
-        max_h = max(max_h, h)
-    answer = max_w * max_h
+        for i in range(len(size)):
+            if max_w < size[0]:
+                max_w = size[0]
+            if max_h < size[1]:
+                max_h = size[1]
+        answer = max_w * max_h
     return answer
