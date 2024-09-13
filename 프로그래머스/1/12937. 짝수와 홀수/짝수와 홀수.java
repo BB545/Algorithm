6339105f5d792d80fac28dea8cce99b31c902d53
노현희 <black1016@hanmail.net>
2024-09-13 12:47:01 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(int num) {
-        String answer = "";
-        if (num % 2 == 0) {
-            answer = "Even";
-        } else {
-            answer = "Odd";
-        }
-        return answer;
+        return (num % 2 == 0) ? "Even" : "Odd";
+    }
+    
+    public static void main(String[] args) {
+        Solution s = new Solution();
     }
 }
