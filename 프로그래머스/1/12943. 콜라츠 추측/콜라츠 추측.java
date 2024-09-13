e55623a5b319820313066ab57bd034de98096e7a
노현희 <black1016@hanmail.net>
2024-09-13 20:44:17 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -2,11 +2,11 @@ class Solution {
     public long solution(long num) {
         long answer = 0;
         while(num != 1) {
-            if (num % 2 == 0) {
-                num = num / 2;
-            } else {
-                num = num * 3 + 1;
-            }
+            num = (num % 2 == 0) ? (
+                num = num / 2
+            ) : (
+                num = num * 3 + 1
+            );
             answer += 1;
             
             if (answer >= 500) {
