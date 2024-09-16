6bf20742df28e707decd1764a83a90ee3cf57abe
노현희 <black1016@hanmail.net>
2024-09-17 00:02:45 +0900

    [level 3] Title: 아이템 줍기, Time: 1.17 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -10,7 +10,10 @@ class Solution {
         int[] dx = new int[]{0, 0, -1, 1};
         int[] dy = new int[]{1, -1, 0, 0};
         
-        boolean[][] visited = new boolean[101][101];
+        int[][] map = new int[101][101];
+        int[][] visited = new int[101][101];
+        boolean[][] check = new boolean[101][101];
+        
         for (int[] point : rectangle) {
             int lx = point[0] * 2;
             int ly = point[1] * 2;
@@ -19,7 +22,7 @@ class Solution {
             
             for (int i = lx; i <= rx; i++) {
                 for (int j = ly; j <= ry; j++) {
-                    visited[i][j] = true;
+                    map[i][j] = 1;
                 }
             }
         }
@@ -32,7 +35,7 @@ class Solution {
             
             for (int i = lx + 1; i < rx; i++) {
                 for (int j = ly + 1; j < ry; j++) {
-                    visited[i][j] = false;
+                    map[i][j] = 0;
                 }
             }
         }
@@ -41,31 +44,27 @@ class Solution {
         int[] start = new int[]{startX, startY};
         que.offer(start);
         
-        int realD = 0;
-        int arriveD = 0;
-        int min = 0;
-        
         while (!que.isEmpty()) {
             int[] now = que.poll();
             int x = now[0];
             int y = now[1];
-            visited[x][y] = false;
             
             if (x == endX && y == endY) {
-                arriveD = realD / 2;
-                min = Math.min(arriveD, (realD - arriveD) / 2);
+                return visited[x][y] / 2;
             }
             
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if (2 <= nx && nx <= 100 && 2 <= ny && ny <= 100 && visited[nx][ny]) {
-                    que.offer(new int[]{nx, ny});
-                }
+                if (nx < 0 || nx > 100 || ny < 0 || ny > 100) continue;
+                if (map[nx][ny] != 1 || check[nx][ny]) continue;
+                
+                check[nx][ny] = true;
+                visited[nx][ny] = visited[x][y] + 1;
+                que.offer(new int[]{nx, ny});
             }
-            realD++;
         }
-        return min;
+        return 0;
     }
 }
