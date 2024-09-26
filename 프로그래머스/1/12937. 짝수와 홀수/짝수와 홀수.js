44c9955bb4c5c34dbb62d44778df9e2b04d7bbcc
노현희 <black1016@hanmail.net>
2024-09-26 22:40:03 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,7 +1,5 @@
 function solution(num) {
     var answer = '';
-    if (num % 2 === 0) {
-        answer = 'Even';
-    } else answer = 'Odd';
+    num % 2 === 0 ? answer = 'Even' : answer = 'Odd';
     return answer;
 }
