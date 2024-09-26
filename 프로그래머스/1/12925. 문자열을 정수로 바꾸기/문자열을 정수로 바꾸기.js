aa1b14aa2f8da973bdb2e8c16e5058d133216eb0
노현희 <black1016@hanmail.net>
2024-09-26 22:32:22 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,6 +1,6 @@
 function solution(s) {
     var answer = 0;
-//     Number(s), parseInt(s), parseFloat(s), +s
+//     Number(s), parseInt(s), parseFloat(s), +s, (s/1 = 자동 변환)
     answer = +s;
     return answer;
 }
