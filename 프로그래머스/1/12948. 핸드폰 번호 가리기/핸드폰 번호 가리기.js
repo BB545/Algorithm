e8c9e2d14eac0ae1c73644ba5de16c0517de8851
노현희 <black1016@hanmail.net>
2024-09-29 17:56:48 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,16 +1,4 @@
 function solution(phone_number) {
-    var answer = [];
-    const phone = phone_number.split("");
-    for (let i = 0; i < phone.length; i++) {
-        if (i + 3 === phone.length - 1) {
-            answer.push(phone[i]);
-            answer.push(phone[i + 1]);
-            answer.push(phone[i + 2]);
-            answer.push(phone[i + 3]);
-            break;
-        } else {
-            answer.push('*');
-        }
-    }
-    return answer.join('');
+    var answer = phone_number.replace(/\d(?=\d{4})/g, '*');
+    return answer;
 }
