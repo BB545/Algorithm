f24a1cb2a6d97d76dc429e5499642634ba4d28eb
노현희 <black1016@hanmail.net>
2024-09-29 15:38:08 +0900

    [level 1] Title: 음양 더하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,10 +1,7 @@
 function solution(absolutes, signs) {
     var answer = 0;
     for (let i = 0; i < signs.length; i++) {
-        if (signs[i] === false) {
-            absolutes[i] = absolutes[i] * (-1);
-        }
-        answer += absolutes[i];
+        signs[i] ? answer += absolutes[i] : answer -= absolutes[i];
     }
     return answer;
 }
