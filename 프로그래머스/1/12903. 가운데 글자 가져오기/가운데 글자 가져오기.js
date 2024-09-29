7fe9920cd4a4be22000c3f41edbb63a3f72c33f6
노현희 <black1016@hanmail.net>
2024-09-29 18:52:03 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,9 +1,6 @@
 function solution(s) {
     var answer = '';
-    if (s.length % 2 === 0) {
-        answer = s.slice(s.length / 2 - 1, s.length / 2 + 1);
-    } else {
-        answer = s.slice(s.length / 2, s.length / 2 + 1);
-    }
+    s.length % 2 === 0 ? answer = s.slice(s.length / 2 - 1, s.length / 2 + 1) : answer = s.slice(s.length / 2, s.length / 2 + 1);
+    
     return answer;
 }
