e986d1c033d987f1eed5ea7f433c92510d3a2a47
노현희 <black1016@hanmail.net>
2024-09-29 18:37:25 +0900

    [level 1] Title: 내적, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,5 @@
 function solution(a, b) {
-    var answer = 0;
-    for (let i = 0; i < a.length; i++) {
-        answer += a[i] * b[i];
-    }
+    var answer = a.reduce((sum, _, i) => sum + a[i] * b[i], 0);
+
     return answer;
 }
