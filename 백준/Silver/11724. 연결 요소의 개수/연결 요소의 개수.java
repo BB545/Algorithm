01fb3b94a8029286c59c4d71ff68e66b494c7226
노현희 <black1016@hanmail.net>
2024-10-19 00:54:09 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1568 ms, Memory: 331536 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+    static List<Integer>[] graph;
+    static boolean[] visited;
+    static int N, M;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        
+        visited = new boolean[N + 1];
+        int count = 0;
+        
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+    
+    public static void dfs (int node) {
+        visited[node] = true;
+        
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
