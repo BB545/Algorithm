32f82cdcd58d5deea67234f3f00b23f727bf5ce8
노현희 <black1016@hanmail.net>
2024-10-21 21:23:36 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 50.26 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,22 +1,11 @@
-import java.util.*;
-
 class Solution {
     public String solution(int n) {
-        String[] list = new String[n];
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
         
         for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                list[i] = "수";
-            } else {
-                list[i] = "박";
-            }
+            answer += (i % 2 == 0) ? "수" : "박";
         }
         
-        for (String word : list) {
-            sb.append(word);
-        }
-        
-        return sb.toString();
+        return answer;
     }
 }
