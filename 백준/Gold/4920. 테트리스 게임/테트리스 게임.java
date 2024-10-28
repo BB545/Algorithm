a9d705f7efa7281d688b3dad03344daa9af77316
노현희 <black1016@hanmail.net>
2024-10-29 02:41:57 +0900

    [Gold IV] Title: 테트리스 게임, Time: 200 ms, Memory: 18420 KB -BaekjoonHub

--- 백준/Gold/4920. 테트리스 게임/테트리스 게임.java
+++ 백준/Gold/4920. 테트리스 게임/테트리스 게임.java
@@ -1,82 +1,79 @@
 import java.io.*;
 import java.util.*;
 
-public class Main
-{
-	
-	static int n;
-	static int[][] map;
-	
-	static int[][][] blocks = {
-			{{0,0}, {0,1}, {0,2}, {0,3}},
-			{{0,0}, {1,0}, {2,0}, {3,0}},
-			{{0,0}, {0,1}, {1,1}, {1,2}},
-			{{0,0}, {1,0}, {1,-1}, {2,-1}},
-			{{0,0}, {0,1}, {0,2}, {1,2}},
-			{{0,0}, {1,0}, {2,0}, {2,-1}},
-			{{0,0}, {1,0}, {1,1}, {1,2}},
-			{{0,0}, {0,1}, {1,0}, {2,0}},
-			{{0,0}, {0,1}, {0,2}, {1,1}},
-			{{0,0}, {1,0}, {1,-1}, {2,0}},
-			{{0,0}, {1,0}, {1,-1}, {1,1}},
-			{{0,0}, {1,0}, {1,1}, {2,0}},
-			{{0,0}, {1,0}, {0,1}, {1,1}}
-	};
-	
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int test_case = 1;
-		
-		while (true) {
-			n = Integer.parseInt(br.readLine().trim());
-			
-			if (n == 0) break;
-			
-			map = new int[n][n];
-			
-			for (int i = 0; i < n; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine().trim()); // trim
-				for (int j = 0; j < n; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken().trim()); // trim
-				}
-			}
-			int maxSum = calMaxSum();
-			
-			sb.append(test_case++).append(". ").append(maxSum).append("\n");
-		}
-		
-		System.out.println(sb);
-	}
-	
-	static int calMaxSum() {
-		int max = Integer.MIN_VALUE;
-		
-		for (int idx = 0; idx < blocks.length; idx++) {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					int sum = calSum(idx, i, j);
-					max = Math.max(sum, max);
-				}
-			}
-		}
-		return max;
-	}
-	
-	static int calSum(int idx, int r, int c) {
-		int sum = 0;
-		for (int i = 0; i < blocks[idx].length; i++) {
-			int nr = r + blocks[idx][i][0];
-			int nc = c + blocks[idx][i][1];
-			
-			if (nr < 0 || nr >= n || nc < 0 || nc >= n) {
-				return Integer.MIN_VALUE;
-			}
-			
-			sum += map[nr][nc];
-		}
-		
-		return sum;
-	}
+public class Main {
+    static int[][] board;
+    static int N;
+    
+    static int[][][] tetris = {
+        {{0,0}, {0,1}, {0,2}, {0,3}},
+		{{0,0}, {1,0}, {2,0}, {3,0}},
+		{{0,0}, {0,1}, {1,1}, {1,2}},
+		{{0,0}, {1,0}, {1,-1}, {2,-1}},
+		{{0,0}, {0,1}, {0,2}, {1,2}},
+		{{0,0}, {1,0}, {2,0}, {2,-1}},
+		{{0,0}, {1,0}, {1,1}, {1,2}},
+		{{0,0}, {0,1}, {1,0}, {2,0}},
+		{{0,0}, {0,1}, {0,2}, {1,1}},
+		{{0,0}, {1,0}, {1,-1}, {2,0}},
+		{{0,0}, {1,0}, {1,-1}, {1,1}},
+		{{0,0}, {1,0}, {1,1}, {2,0}},
+		{{0,0}, {1,0}, {0,1}, {1,1}}
+    };
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int testCase = 1;
+        
+        while (true) {
+            N = Integer.parseInt(br.readLine().trim());
+            if (N == 0) break;
+            
+            board = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine().trim());
+                
+                for (int j = 0; j < N; j++) {
+                    board[i][j] = Integer.parseInt(st.nextToken().trim());
+                }
+            }
+            
+            int maxSum = Integer.MIN_VALUE;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    maxSum = checkTetris(i, j, maxSum);
+                }
+            }
+            
+            sb.append(testCase++).append(". ").append(maxSum).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+    
+    public static int checkTetris(int x, int y, int maxSum) {        
+        for (int[][] shape : tetris) {
+            int sum = 0;
+            boolean isValid = true;
+            
+            for (int[] s : shape) {
+                int nx = x + s[0];
+                int ny = y + s[1];
+                
+                if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                    sum += board[nx][ny];
+                } else {
+                    isValid = false;
+                    break;
+                }
+            }
+            
+            if (isValid) {
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    }
 }
