4e2db2fef7e8b8953ac8b94fe332f5f02454289d
노현희 <black1016@hanmail.net>
2024-10-31 23:36:44 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-        
-        if (s.length() != 4 && s.length() != 6) answer = false;
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isLetter(s.charAt(i))) {
-                answer = false;
-                break;
+        if (s.length() == 4 || s.length() == 6) {
+            try {
+                int x = Integer.parseInt(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
             }
-        }
-        
-        return answer;
+        } else return false;
     }
 }
