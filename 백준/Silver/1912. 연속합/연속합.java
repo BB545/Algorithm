0e5b4bcd33c6198a14d8aa5e19117ed701bcce36
HYUNHEE NOH <black1016@hanmail.net>
2025-01-03 17:24:26 +0900

    [Silver II] Title: 연속합, Time: 232 ms, Memory: 23632 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine().trim());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+        
+        for (int i = 1; i < n; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        System.out.println(maxSum);
+    }
+}
