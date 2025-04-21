4034a5274b9972f8491e44e8d6af30f888781e02
HYUNHEE NOH <black1016@hanmail.net>
2025-04-21 15:47:38 +0900

    [Silver I] Title: 데스 나이트, Time: 112 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Silver/16948. 데스 나이트/데스 나이트.java
+++ 백준/Silver/16948. 데스 나이트/데스 나이트.java
@@ -2,16 +2,13 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static boolean[][] visited;
     static int N;
-    static int[][] dist;
     static int[] dx = {-2, -2, 0, 0, 2, 2};
     static int[] dy = {-1, 1, -2, 2, -1, 1};
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine().trim());
-        
+        N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         int r1 = Integer.parseInt(st.nextToken());
         int c1 = Integer.parseInt(st.nextToken());
@@ -25,25 +22,26 @@ class Main {
         if (startX == endX && startY == endY) return 0;
         
         Queue<int[]> que = new LinkedList<>();
-        visited = new boolean[N][N];
-        dist = new int[N][N];
+        int[][] dist = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(dist[i], -1);
+        }
         
         que.add(new int[]{startX, startY});
-        visited[startX][startY] = true;
+        dist[startX][startY] = 0;
         
         while(!que.isEmpty()) {
             int[] current = que.poll();
-            int x = current[0];
-            int y = current[1];
+            int cx = current[0];
+            int cy = current[1];
             
             for (int i = 0; i < 6; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
                 
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[nx][ny]) {
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && dist[nx][ny] == -1) {
                     que.add(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                    dist[nx][ny] = dist[x][y] + 1;
+                    dist[nx][ny] = dist[cx][cy] + 1;
                     
                     if (nx == endX && ny == endY) {
                         return dist[nx][ny];
