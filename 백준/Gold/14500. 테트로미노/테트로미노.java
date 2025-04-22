1801d54d6958ef640f6308299ef96052b7ab3334
HYUNHEE NOH <black1016@hanmail.net>
2025-04-23 01:53:10 +0900

    [Gold IV] Title: 테트로미노, Time: 644 ms, Memory: 34252 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,49 +1,51 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
     static int N, M;
-    static int[][] board;
+    static int[][] map;
+    static int max = 0;
     static boolean[][] visited;
-    static int maxSum = 0;
-    
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    
-    static int[][][] tetromino = {
-        {{0, 0}, {0, 1}, {0, 2}, {1, 1}},
-        {{0, 0}, {1, 0}, {1, 1}, {2, 0}},
-        {{0, 1}, {1, 0}, {1, 1}, {2, 1}},
-        {{0, 1}, {1, 0}, {1, 1}, {1, 2}},
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
+    static int[][][] tetrominoEtc = {
+        {{0, 0}, {-1, 0}, {0, -1}, {0, 1}},
+        {{0, 0}, {-1, 0}, {0, 1}, {1, 0}},
+        {{0, 0}, {0, -1}, {0, 1}, {1, 0}},
+        {{0, 0}, {0, -1}, {-1, 0}, {1, 0}},
     };
     
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         
-        board = new int[N][M];
+        map = new int[N][M];
         visited = new boolean[N][M];
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            
             for (int j = 0; j < M; j++) {
-                board[i][j] = sc.nextInt();
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 visited[i][j] = true;
-                dfs(i, j, 0, 0);
+                dfs(i, j, map[i][j], 1);
                 visited[i][j] = false;
-                checkTetromino(i, j);
+                checkEtc(i, j);
             }
         }
         
-        System.out.println(maxSum);
+        System.out.println(max);
     }
     
-    public static void dfs(int x, int y, int depth, int sum) {
-        if (depth == 4) {
-            maxSum = Math.max(maxSum, sum);
+    public static void dfs(int x, int y, int sum, int cnt) {
+        if (cnt == 4) {
+            max = Math.max(max, sum);
             return;
         }
         
@@ -51,33 +53,33 @@ public class Main {
             int nx = x + dx[i];
             int ny = y + dy[i];
             
-            if (nx >= 0 && ny >= 0 && nx < N && ny < M && !visited[nx][ny]) {
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+            if (!visited[nx][ny]) {
                 visited[nx][ny] = true;
-                dfs(nx, ny, depth + 1, sum + board[nx][ny]);
+                dfs(nx, ny, sum + map[nx][ny], cnt + 1);
                 visited[nx][ny] = false;
             }
         }
     }
     
-    public static void checkTetromino(int x, int y) {
-        for (int[][] shape : tetromino) {
+    public static void checkEtc(int x, int y) {
+        for (int[][] tetromino : tetrominoEtc) {
             int sum = 0;
             boolean isValid = true;
+        
+            for(int[] tetro : tetromino) {
+                int nx = x + tetro[0];
+                int ny = y + tetro[1];
             
-            for (int[] s : shape) {
-                int nx = x + s[0];
-                int ny = y + s[1];
-                
-                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
-                    sum += board[nx][ny];
-                } else {
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
                     isValid = false;
                     break;
                 }
+                sum += map[nx][ny];
             }
             
             if (isValid) {
-                maxSum = Math.max(maxSum, sum);
+                max = Math.max(max, sum);
             }
         }
     }
