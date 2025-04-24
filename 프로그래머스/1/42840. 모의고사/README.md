f4b0deef1d70a19631b8337b1b5af50894c7b7c1
HYUNHEE NOH <black1016@hanmail.net>
2025-04-24 17:34:55 +0900

    [level 1] Title: 모의고사, Time: 19.34 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 77.6 MB, 시간: 3.42 ms
+메모리: 88.2 MB, 시간: 19.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 18일 23:51:11
+2025년 04월 24일 17:34:48
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,39 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] s1 = new int[]{1,2,3,4,5};
-        int[] s2 = new int[]{2,1,2,3,2,4,2,5};
-        int[] s3 = new int[]{3,3,1,1,2,2,4,4,5,5};
+        int[] p1 = {1,2,3,4,5};
+        int[] p2 = {2,1,2,3,2,4,2,5};
+        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
         
-        List<Integer> score = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        
-        int cnt1 = 0, cnt2 = 0, cnt3 = 0;
-        
+        int[] score = new int[3];
         for (int i = 0; i < answers.length; i++) {
-            if (s1[i % s1.length] == answers[i]) {
-                cnt1++;
-            }
-            if (s2[i % s2.length] == answers[i]) {
-                cnt2++;
-            }
-            if (s3[i % s3.length] == answers[i]) {
-                cnt3++;
+            if (answers[i] == p1[i % p1.length]) score[0]++;
+            if (answers[i] == p2[i % p2.length]) score[1]++;
+            if (answers[i] == p3[i % p3.length]) score[2]++;
+        }
+        
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            if (max == score[i]) {
+                answer.add(i + 1);
             }
         }
         
-        score.add(cnt1);
-        score.add(cnt2);
-        score.add(cnt3);
-        
-        int max = Collections.max(score);
-        
-        for (int i = 0; i < score.size(); i++) {
-            if (max == score.get(i)) {
-                ans.add(i + 1);
-            }
-        }
-        
-        return ans.stream().mapToInt(i -> i).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
