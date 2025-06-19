36d48c60856deef53e4551e1e7eec908d8f8b897
HYUNHEE NOH <black1016@hanmail.net>
2025-06-19 15:25:13 +0900

    [level 3] Title: 정수 삼각형, Time: 15.80 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,12 +1,23 @@
+import java.util.*;
+
 class Solution {
+    static int[][] dp;
+    
     public int solution(int[][] triangle) {
-        int H = triangle.length;
+        int answer = 0;
+        int n = triangle.length;
+        dp = new int[n][n];
         
-        for (int i = H - 2; i >= 0; i--) { // 거꾸로 최댓값 갱신, 누적합 저장, i를 마지막에서 2번째줄부터 시작
-            for (int j = 0; j < triangle[i].length; j++) {
-                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]); // 마지막줄 누적합 기본값으로 활용하기 위해
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = triangle[n - 1][i];
+        }
+        
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
             }
         }
-        return triangle[0][0]; // 최상위 노드가 최댓값
+        answer = dp[0][0];
+        return answer;
     }
 }
