aa1e3b8fc20a2d4e78277cc823162ca46d742110
HYUNHEE NOH <black1016@hanmail.net>
2025-06-20 16:23:54 +0900

    [level 2] Title: 전화번호 목록, Time: 407.75 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(phone_book); // 사전 순 정렬
         
         for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) { // 인접한 항목 비교
                 return false;
             }
         }
