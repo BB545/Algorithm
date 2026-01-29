29ed9b33ccef3194d66557397945dbc513a6fe85
HYUNHEE NOH <black1016@hanmail.net>
2026-01-30 00:36:19 +0900

    Time: 2 ms (99.14%), Space: 47.3 MB (16.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
-
         Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
             int op = target - nums[i];
             if (map.containsKey(op)) {
-                answer[0] = map.get(op);
-                answer[1] = i;
+                return new int[] {map.get(op), i};
             }
             map.put(nums[i], i);
         }
 
-        return answer;
+        return new int[] {};
     }
 }
