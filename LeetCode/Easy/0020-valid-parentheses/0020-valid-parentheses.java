ec6a2a366bf971b57f8c397abe1aa8482e8e21be
BB545 <black1016@hanmail.net>
2026-01-30 16:00:38 +0000

    chore: organize LeetHub folders

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,27 +2,20 @@ class Solution {
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
-                stack.push(s.charAt(i));
+        for (Character c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
             } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                Character last = stack.pop();
-
-                if (last == '(' && s.charAt(i) == ')' || last == '{' && s.charAt(i) == '}' || last == '[' && s.charAt(i) == ']') {
-                    continue;
-                } else {
+                if (stack.isEmpty() || stack.pop() != c) {
                     return false;
                 }
             }
         }
 
-        if (stack.isEmpty()) {
-            return true;
-        }
-
-        return false;
+        return stack.isEmpty();
     }
 }
