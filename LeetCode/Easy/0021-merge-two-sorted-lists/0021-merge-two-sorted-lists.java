a0cfbfd0506ebdd30c5de24810f2a2195fc2c049
BB545 <black1016@hanmail.net>
2026-01-31 15:46:57 +0000

    chore: organize LeetHub folders

--- LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,22 +10,20 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                current.next = list1;
-                list1 = list1.next;
+        if (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1; // 현재 단계 head 반환
             } else {
-                current.next = list2;
-                list2 = list2.next;
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
             }
-            current = current.next;
         }
 
-        current.next = (list1 != null) ? list1 : list2;
+        if (list1 == null) {
+            return list2;
+        }
 
-        return dummy.next;
+        return list1;
     }
 }
